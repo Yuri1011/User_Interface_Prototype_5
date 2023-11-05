981bb0585c99182cc356f5493f079f48357a7113
Yury Trypolski <yury1011.89@gmail.com>
2023-11-05 16:31:39 +0300

    added random force and rotate and position for objects

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour {
+    private Rigidbody targetRb;
     void Start() {
-        
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.Range(14, 17), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -3);
     }
 
     void Update() {
