a4cddfcb0e2b7cb724813c2b2717c5f4e54075ed
Yury Trypolski <yury1011.89@gmail.com>
2023-11-09 11:54:48 +0300

    added text Mesh Pro for score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
     void Start() {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Scorer: " + score;
     }
     IEnumerator SpawnTarget() {
         while (true) {
